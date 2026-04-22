b6f8c6c197bccd720b1ab9572de9dd7cc798ad50
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-04-22 09:57:44 -0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
+import sys 
+from todo_manager import read_todo_file, write_todo_file
 
-# TODO: Implementar CLI según README.md
+def main():
+            try:
+                if len(sys.argv) < 2:
+                 raise IndexError("Insufficient arguments provided!")
+
+
