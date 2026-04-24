6d70f658a6c1b7ac0c3d306db6d4db80f5da4ce4
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-04-24 10:40:18 -0500

    main.py

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ from todo_manager import read_todo_file, write_todo_file
 try:
     if len(sys.argv) < 2:
         raise IndexError("Insufficient arguments provided!")
-    if sys.argv[1] == "--help":
-            print("""Usage: python main.py <file_path> <command> [arguments]...
-Commands:
-  add "task"     - Add a task to the list.
-  remove "task"  - Remove a task from the list.
-  view           - Display all tasks.
-Examples:
-  python main.py tasks.txt add "Buy groceries"
-  python main.py tasks.txt remove "Do laundry"
-  python main.py tasks.txt view
-  python main.py tasks.txt add "Call mom" remove "Take out trash" view""")
-        return
+    arguments = sys.argv[1:]
+    file = arguments[0]
 
-    file_path = sys.argv[1]
-    tasks = read_todo_file(file_path)
+    tasks = read_todo_file(file)
+
+    print("Command-line arguments:")
+    for arg in arguments:
+        print(arg)
+    print("\nTasks:")
+    for task in tasks:
+        print(task)
+except IndexError as e:
+    print(e)
 
     
 
