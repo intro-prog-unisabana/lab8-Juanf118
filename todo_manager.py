56d81d397711f83fcaa90f91619c2f8ecc7db3d7
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-04-25 17:27:18 -0500

    main.py

--- todo_manager.py
+++ todo_manager.py
@@ -4,16 +4,17 @@
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             return file.read().splitlines()
-        
+
     except FileNotFoundError:
         print(f"File {file_path} not found! Returning an empty to-do list.")
+        open(file_path, 'w', encoding='utf-8').close()
         return []
 
 
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
-    with open(file_path, 'w') as file:
-            for task in tasks:
-                file.write(f"{task}\n")
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for task in tasks:
+            file.write(f"{task}\n")
